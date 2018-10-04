69028cfb528c88bb0a53874a0038b70cbab1a68f
academy_intern <academy_intern@academy-interns-MacBook-Pro.local>
2018-10-04 11:56:10 +0200

    initial commit'

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,21 @@
-import * as express from 'express';
-import * as bodyParser from 'body-parser'; //used to parse the form data that you pass in the request
+import * as express from "express"
+import * as bodyParser from "body-parser"
+import {Pokemons} from "./routes/pokemons"
 
-class App {
-
-    public app: express.Application;
+class App{
+    public app: express.Application
+    public pokeRoutes: Pokemons = new Pokemons()
 
     constructor() {
-        this.app = express(); //run the express instance and store in app
-        this.config();
+        this.app = express()
+        this.config()
+        this.pokeRoutes.routes(this.app)
     }
 
-    private config(): void {
-        // support application/json type post data
-        this.app.use(bodyParser.json());
-        //support application/x-www-form-urlencoded post data
-        this.app.use(bodyParser.urlencoded({
-            extended: false
-        }));
+    private config(): void{
+        this.app.use(bodyParser.json())
+        this.app.use(bodyParser.urlencoded({extended: false}))
     }
-
 }
 
-export default new App().app;
+export default new App().app
